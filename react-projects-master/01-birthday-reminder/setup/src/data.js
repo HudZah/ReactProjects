63c29ea1561cc1e0b7f271f00320a41a2874af73
hudzah <chiefhudhayfa@gmail.com>
2022-03-26 00:38:14 -0400

    new

---
+++ react-projects-master/01-birthday-reminder/setup/src/data.js
@@ -0,0 +1,50 @@
+export default [
+	{
+		id: 1,
+		name: "Simon Chase",
+		age: 29,
+		image: "https://i.ibb.co/tXh7k1d/Screenshot-2022-03-26-at-12-22-07-AM.png",
+	},
+	{
+		id: 2,
+		name: "Kabir Wahi",
+		age: 32,
+		image: "https://i.ibb.co/XD9mqW6/IMG-0273.jpg",
+	},
+	{
+		id: 3,
+		name: "Thanh Le",
+		age: 36,
+		image: "https://i.ibb.co/qB14WRj/IMG-0413.jpg",
+	},
+	{
+		id: 4,
+		name: "Xavier Maier",
+		age: 34,
+		image: "https://i.ibb.co/HxSPWGW/IMG-0324.jpg",
+	},
+	{
+		id: 5,
+		name: "HudZah",
+		age: 29,
+		image: "https://i.ibb.co/2FKHx3j/IMG-0254.jpg",
+	},
+	{
+		id: 6,
+		name: "Alex Boden",
+		age: 29,
+		image: "https://i.ibb.co/YN9dfWb/IMG-0132.jpg",
+	},
+	{
+		id: 7,
+		name: "Kyle O'Conner",
+		age: 29,
+		image: "https://i.ibb.co/MBNLBk0/Screenshot-2022-03-26-at-12-16-07-AM.png",
+	},
+	{
+		id: 8,
+		name: "Ali Al Humidi",
+		age: 29,
+		image: "https://i.ibb.co/fqYNpnQ/IMG-0030.png",
+	},
+];
