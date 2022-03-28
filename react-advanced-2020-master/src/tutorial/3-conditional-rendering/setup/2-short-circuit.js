e5a3161f73af4eb8fced5749effc84efca38bfa2
hudzah <chiefhudhayfa@gmail.com>
2022-03-27 21:44:55 -0400

    window resize

--- react-advanced-2020-master/src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ react-advanced-2020-master/src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -1,12 +1,23 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 // short-circuit evaluation
 // ternary operator
 
 const ShortCircuit = () => {
-  // const firstValue = text || 'hello world';
-  // const secondValue = text && 'hello world';
+	const [text, setText] = useState("");
+	const [isError, setIsError] = useState(false);
+	// const firstValue = text || "hello world";
+	// const secondValue = text && "hello world";
 
-  return <h2>short circuit</h2>;
+	return (
+		<>
+			<h1>{text || "john doe"}</h1>
+			<button className="btn" onClick={() => setIsError(!isError)}>
+				Toggle Error
+			</button>
+			{isError && <h2>Error</h2>}
+			{isError ? <h2>Error</h2> : null}
+		</>
+	);
 };
 
 export default ShortCircuit;
