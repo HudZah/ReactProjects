c1c4eadb403ab335220bfad383350d941c072a30
hudzah <chiefhudhayfa@gmail.com>
2022-03-29 23:17:28 -0400

    reviews new

--- react-projects-master/03-reviews/setup/src/data.js
+++ react-projects-master/03-reviews/setup/src/data.js
@@ -1,40 +1,32 @@
 const reviews = [
-  {
-    id: 1,
-    name: 'susan smith',
-    job: 'web developer',
-    image:
-      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
-    text:
-      "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
-  },
-  {
-    id: 2,
-    name: 'anna johnson',
-    job: 'web designer',
-    image:
-      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883409/person-2_np9x5l.jpg',
-    text:
-      'Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.',
-  },
-  {
-    id: 3,
-    name: 'peter jones',
-    job: 'intern',
-    image:
-      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
-    text:
-      'Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.',
-  },
-  {
-    id: 4,
-    name: 'bill anderson',
-    job: 'the boss',
-    image:
-      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg',
-    text:
-      'Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ',
-  },
+	{
+		id: 1,
+		name: "Nachos",
+		job: "Dinner",
+		image: "https://i.ibb.co/pnQJJbF/Screenshot-2022-03-29-at-11-07-59-PM.png",
+		text: "🌟🌟🌟🌟 Nachos These go really hard. Another food fare classic. You really cant ask for more in a bowl of nachos.",
+	},
+	{
+		id: 2,
+		name: "Quesadilla",
+		job: "Lunch",
+		image: "https://i.ibb.co/vDzFTCX/Screenshot-2022-03-29-at-11-08-37-PM.png",
+		text: "🌟🌟🌟🌟 Pulled Pork Quesadilla. They had pulled pork, so I asked Pernell to make me something good with it. He did not disappoint. I dont recall what he put in it, but it goes hard.",
+	},
+	{
+		id: 3,
+		name: "Lazeez",
+		job: "Any",
+		image: "https://i.ibb.co/r65hBbj/Screenshot-2022-03-29-at-11-08-49-PM.png",
+		text: "🌟🌟🌟🌟🌟 Unknown Substance Had to make a new rating for this. Its an unedited picture from [REDACTED]’s fridge. Our best guess for its origin is Lazeez.",
+	},
+	{
+		id: 4,
+		name: "Mug",
+		job: "Mug",
+		image: "https://i.ibb.co/CPSQqN6/Screenshot-2022-03-29-at-11-09-14-PM.png",
+		text: "⭐️⭐️⭐️ Mug in a mug Goes hard",
+	},
 ];
 
 export default reviews;
