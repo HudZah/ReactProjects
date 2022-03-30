a622d6dba9f6b5f7fecf93e4a5d2023d43ea5e39
hudzah <chiefhudhayfa@gmail.com>
2022-03-30 08:36:23 -0400

    menu-app

---
+++ react-projects-master/05-menu/setup/src/Categories.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Categories = ({ filterItems, categories }) => {
+	return (
+		<div className="btn-container">
+			{categories.map((item) => {
+				return (
+					<button
+						className="filter-btn"
+						onClick={() => filterItems(item)}
+					>
+						{item}
+					</button>
+				);
+			})}
+		</div>
+	);
+};
+
+export default Categories;
