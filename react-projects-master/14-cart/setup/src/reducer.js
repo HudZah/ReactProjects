bb0c822910625152899bb87b5cab5bfaa2fe7955
hudzah <chiefhudhayfa@gmail.com>
2022-04-24 00:42:26 -0400

    dec/inc items

--- react-projects-master/14-cart/setup/src/reducer.js
+++ react-projects-master/14-cart/setup/src/reducer.js
@@ -6,6 +6,43 @@ const reducer = (state, action) => {
 			total: 1110,
 		};
 	}
+
+	if (action.type === "REMOVE") {
+		return {
+			...state,
+			cart: state.cart.filter((item) => item.id !== action.payload),
+		};
+	}
+
+	if (action.type === "INCREASE") {
+		let tempCart = state.cart.map((item) => {
+			if (item.id === action.payload) {
+				return {
+					...item,
+					amount: item.amount + 1,
+				};
+			}
+			return item;
+		});
+		return { ...state, cart: tempCart };
+	}
+
+	if (action.type === "DECREASE") {
+		let tempCart = state.cart
+			.map((cartItem) => {
+				if (cartItem.id === action.payload) {
+					return {
+						...cartItem,
+						amount: cartItem.amount - 1,
+					};
+				}
+
+				return cartItem;
+			})
+			.filter((cartItem) => cartItem.amount > 0);
+		return { ...state, cart: tempCart };
+	}
+
 	return state;
 };
 
